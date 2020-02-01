599cb660f4e161118f52a0fcb146190b889c425a
Khalid Osama <khalidosama@msn.com>
2020-02-02 01:30:40 +0800

    Build basic add topic layout Refactor other code

--- app/src/main/java/com/osama/reddittest/addtopics/AddTopicFragment.kt
+++ app/src/main/java/com/osama/reddittest/addtopics/AddTopicFragment.kt
@@ -1,13 +1,10 @@
 package com.osama.reddittest.addtopics
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.osama.reddittest.R
 
 /**
@@ -20,7 +17,7 @@ class AddTopicFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_topic, container, false)
+        return inflater.inflate(R.layout.add_topic_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
