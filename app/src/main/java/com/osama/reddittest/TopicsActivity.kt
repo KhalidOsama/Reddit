b53d8be6a312ff7b2af2138d34207dc62d81f623
Khalid Osama <khalidosama@msn.com>
2020-02-01 14:19:45 +0800

    Set up AppBar Modify theme to remove ActionBar Setup default fragment destination

--- app/src/main/java/com/osama/reddittest/TopicsActivity.kt
+++ app/src/main/java/com/osama/reddittest/TopicsActivity.kt
@@ -2,18 +2,34 @@ package com.osama.reddittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.osama.reddittest.ui.topics.TopicsFragment
 
 class TopicsActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.topics_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, TopicsFragment.newInstance())
-                .commitNow()
-        }
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.topicsFragment)
+                .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 }
