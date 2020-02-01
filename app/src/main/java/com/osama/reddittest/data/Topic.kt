51fd4011a1a47d9a55050755e6747638f6775242
Khalid Osama <khalidosama@msn.com>
2020-02-01 20:03:04 +0800

    Create Topic model

---
+++ app/src/main/java/com/osama/reddittest/data/Topic.kt
@@ -0,0 +1,10 @@
+package com.osama.reddittest.data
+
+/**
+ * Class which provides a model for a Topic
+ * @constructor Sets all properties of the topic
+ * @property body the content of the topic
+ * @property upVotes number of up votes
+ * @property downVotes number of down votes
+ */
+data class Topic(val body: String, val upVotes: Int, val downVotes: Int)
