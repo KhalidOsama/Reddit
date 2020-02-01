5085a64717e642bf55d3ca6ca3fb7b0d61fb080a
Khalid Osama <khalidosama@msn.com>
2020-02-02 03:54:26 +0800

    Implement a few UI improvements Improve UX with a success and fail messages when adding a topic, and navigting back to the list automatically in case of success

--- app/src/main/java/com/osama/reddittest/addtopics/AddTopicFragment.kt
+++ app/src/main/java/com/osama/reddittest/addtopics/AddTopicFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.osama.reddittest.R
 import com.osama.reddittest.data.Topic
+import com.osama.reddittest.topicslist.TopicsListFragmentDirections
 import com.osama.reddittest.topicslist.TopicsListViewModel
 import kotlinx.android.synthetic.main.add_topic_fragment.*
 import kotlinx.android.synthetic.main.topics_fragment.*
+import kotlinx.coroutines.flow.callbackFlow
 import java.util.*
 
 /**
@@ -43,11 +47,13 @@ class AddTopicFragment : Fragment() {
         activity?.findViewById<FloatingActionButton>(R.id.fab_save_topic)?.let {
             it.setOnClickListener {
                 when (et_topic_body.text?.length?:0){
+//                    If no content prompt
                     0 -> Snackbar.make(
                         it, // Parent view
                         "Kindly enter a topic", // Message to show
                         Snackbar.LENGTH_SHORT // How long to display the message.
                     ).show()
+//                    If there is content, add it to the view model and navigate back to the list
                     else -> {
                         viewModel.addTopic(
                             Topic(
@@ -55,15 +61,21 @@ class AddTopicFragment : Fragment() {
                                 et_topic_body.text.toString(),
                                 Calendar.getInstance().timeInMillis
                             ))
-                        Snackbar.make(
-                            it, // Parent view
-                            "Topic entered successfully", // Message to show
-                            Snackbar.LENGTH_SHORT // How long to display the message.
-                        ).show()
+                        Toast.makeText(activity,
+                            "Topic entered successfully",
+                            Toast.LENGTH_SHORT)
+                            .show()
+                        navigateToTopicsList()
                     }
                 }
             }
         }
+    }
+
+    private fun navigateToTopicsList() {
+        val action = AddTopicFragmentDirections
+            .actionAddTopicFragmentToTopicsFragment()
+        findNavController().navigate(action)
     }
 
 }
